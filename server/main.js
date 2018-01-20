922392545caf4f25bfbe74f90a63d6e918b2c1ce
duanbaihong <duanbaihong@qq.com>
2018-01-20 15:58:02 +0800

    add

---
+++ server/main.js
@@ -0,0 +1,34 @@
+
+'use strict';
+
+const {app, Menu, ipcMain} = require('electron')
+const windowManager = require('./windowManager')
+const menu = require('./menu')
+
+ipcMain.on('create patternManager', function (event, arg) {
+  windowManager.create('patternManager', arg);
+});
+
+ipcMain.on('dispatch', function (event, action, arg) {
+  windowManager.dispatch(action, arg);
+});
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function (e, hasVisibleWindows) {
+  if (!hasVisibleWindows) {
+    windowManager.create();
+  }
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function () {
+  Menu.setApplicationMenu(menu);
+  windowManager.create();
+});
